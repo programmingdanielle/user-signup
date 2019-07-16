a65781bb8c70aa5a914add6cb7fa05670755a8cf
Danielle <danibernhoester@gmail.com>
2019-07-16 10:57:51 -0500

    tweaked the login errors

--- main.py
+++ main.py
@@ -14,29 +14,56 @@ def verify_info():
     password = request.form["password"]
     verify = request.form["verify"]
     email = request.form["email"]
-    if whitespace(username) or whitespace(password) or whitespace(email) == True:
-        error = "Please use no white spaces."
+    user_blank = "Please enter a username."
+    field_blank = "Please fill in the required fields."
+    error = "Please use no white spaces in your username."
+    pwerror = "Your passwords don't match."
+    pass_blank = "Please fill in your password."
+    pw_short_error = "Your password must be longer then 3 characters."
+    pw_long_error = "Your password must be less then 20 characters."
+    extra_email_error = "You can only have one @ or . in your email."
+    missing_email_error = "You're missing a @ or a . in your email."
+    user_short_error = "Your username must be longer then 3 characters."
+    user_long_error = "Your username name must be less then 20 characters."
+    pwwerror = "Please use no white spaces in your password."
+    emerror = "Please use no white spaces in your email."
+    em_too_short = "Your email must be at least 3 characters long."
+    em_too_long = "Your email must be at least 20 characters long."
+    if whitespace(username) == True:
         return render_template("index.html", error=error, username=username, email=email)
+    if whitespace(password) == True:
+        return render_template("index.html", pwwerror=pwwerror, username=username, email=email)
+    if whitespace(email) == True:
+        return render_template("index.html", emerror=emerror, username=username, email=email)
+    if username == "" and password == "":
+        return render_template("index.html", field_blank=field_blank, username=username, email=email)
+    if username == "":
+        return render_template("index.html", user_blank=user_blank, username=username, email=email)
+    if password == "":
+        return render_template("index.html", pass_blank=pass_blank, username=username, email=email)
+    if len(username) < 3:
+        return render_template("index.html", user_short_error=user_short_error, username=username, email=email)
+    if len(username) > 20:
+        return render_template("index.html", user_long_error=user_long_error, username=username, email=email)
     if verify_password(password, verify) == False:
-        pwerror = "Your passwords don't match."
-        return render_template("index.html", error=pwerror, username=username, email=email)
+        return render_template("index.html", pwerror=pwerror, username=username, email=email)
     if verify_password(password, verify) == True:
         if len(password) < 3:
-            pw_short_error = "Your password must be longer then 3 characters."
-            return render_template("index.html", error=pw_short_error, username=username, email=email)
+            return render_template("index.html", pw_short_error=pw_short_error, username=username, email=email)
         if len(password) > 20:
-            pw_long_error = "Your password must be less then 20 characters."
-            return render_template("index.html", error=pw_long_error, username=username, email=email)
+            return render_template("index.html", pw_long_error=pw_long_error, username=username, email=email)
     if email is not "":
         if verify_email(email) == False:
-            missing_email_error = "You're missing a @ or a . in your email."
-            return render_template("index.html", error=missing_email_error, username=username, email=email)
+            return render_template("index.html", missing_email_error=missing_email_error, username=username, email=email)
+        if len(email) < 3:
+            return render_template("index.html", em_too_short=em_too_short, username=username, email=email)
+        if len(email) > 20:
+            return render_template("index.html", em_too_long=em_too_long, username=username, email=email)
         if verify_email(email) == True:
             if ifemail(email) == True:
                 return redirect("/welcome?username={0}".format(username))
             if ifemail(email) == False:
-                extra_email_error = "You can only have one @ or . in your email."
-                return render_template("index.html", error=extra_email_error, username=username, email=email)
+                return render_template("index.html", extra_email_error=extra_email_error, username=username, email=email)
     else:
         return redirect("/welcome?username={0}".format(username))
     
